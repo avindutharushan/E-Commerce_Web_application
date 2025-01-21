e6d9a62ab0c1f7c30f0e4ca0d39aee38e0622ad5
avindutharushan <avindutharushan2002@gmail.com>
2025-01-21 13:52:33 +0530

    The task of logout user is complete.

---
+++ src/main/java/lk/ijse/ecommerceplatform/LogoutServlet.java
@@ -0,0 +1,34 @@
+package lk.ijse.ecommerceplatform;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+
+@WebServlet(name = "logout", value = "/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            session.removeAttribute("user");
+            session.removeAttribute("userName");
+            session.removeAttribute("userEmail");
+
+            session.invalidate();
+        }
+
+        response.sendRedirect("index.jsp");
+    }
+
+
+}
